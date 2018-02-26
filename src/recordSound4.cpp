5c1d00af4ff2b5ce480b8a5b4ae1fce010493ce1
mililivantilili <matej.kaloc@gmail.com>
2018-02-26 19:35:57 +0100

    Second commit

--- src/recordSound4.cpp
+++ src/recordSound4.cpp
@@ -20,21 +20,39 @@
 using namespace std;
 
 #define SAMPLE_NUM 10000
+#define REPRO 25
+#define LED 21
+#define GET_DATA 1
+#define SET_RES 2
+#define SPI_SETUP 3
+#define CS0 10
+#define CS1 11
+#define CS2 3
 
 static const int CHANNEL = 0;	// Channel of SPI (Depending on how it is connected)
 
 void* sound( void* arg );
-void* getSpiData( void* arg );
+void* manageThread( void* arg );
 void endMelody(void);
-int SpiSetup(void);
+int SpiSetup();
+int GetSpiData(void);
+int SetRes(void);
 
 bool shutdown = true;
 
+long count = 0;
+int stav = SPI_SETUP;
+
+unsigned char buffer[2];
+//buffer[0] = 16;
 unsigned short result[SAMPLE_NUM];
 
-long count = 0;
+struct timeval current;
+struct timeval start;
+long us;
+long us2;
 
-void timer_handler (int signum)
+/*void timer_handler (int signum)
 {
 
 	count += 1;
@@ -46,21 +64,21 @@ void timer_handler (int signum)
 		printf ("%d.%06d: timer expired %d times\n", ts.tv_sec, ts.tv_usec, count);
 		count = 0;
 	}
-}
+}*/
 
 int main() {
 	printf ("%s \n", "Starting program...");
 
 	wiringPiSetup () ;
-	pinMode (6, OUTPUT) ;//svetlo
-	pinMode (7, OUTPUT) ;//reprak
+	pinMode (LED, OUTPUT) ;//svetlo
+	pinMode (REPRO, OUTPUT) ;//reprak
 	//for (;;)
 	//{
-		//digitalWrite (6, HIGH) ;
-		//digitalWrite (7, HIGH) ;
-		//delay (2) ;
-	    //digitalWrite (6,  LOW) ;
-	   //digitalWrite (7,  LOW) ;
+		//digitalWrite (LED, HIGH) ;
+		//digitalWrite (REPRO, HIGH) ;
+		//delay (1000) ;
+	    //digitalWrite (LED,  LOW) ;
+	   //digitalWrite (REPRO,  LOW) ;
 	   // delay (2) ;
 	//}
 
@@ -68,27 +86,27 @@ int main() {
 	pthread_create( &sound_thread, NULL, &sound, NULL );
 
 	//SPI inicializing
-	if(SpiSetup() == 0)
-	{
-		 struct sigaction sa;
-		 struct itimerval timer;
+	/*if(SpiSetup(CHANNEL) == 0)
+	{*/
+		 /*struct sigaction sa;
+		 struct itimerval timer;/**/
 		 /* Install timer_handler as the signal handler for SIGVTALRM. */
-		 memset (&sa, 0, sizeof (sa));
+		/* memset (&sa, 0, sizeof (sa));
 		 sa.sa_handler = &timer_handler;
-		 sigaction (SIGVTALRM, &sa, NULL);
+		 sigaction (SIGVTALRM, &sa, NULL);*/
 		 /* Configure the timer to expire after 500 msec... */
-		  timer.it_value.tv_sec = 0;
-		  timer.it_value.tv_usec = 500000;
+		  /*timer.it_value.tv_sec = 0;
+		  timer.it_value.tv_usec = 500000;*/
 		  /* ... and every 500 msec after that. */
-		  timer.it_interval.tv_sec = 0;
-		  timer.it_interval.tv_usec = 500000;
+		  /*timer.it_interval.tv_sec = 0;
+		  timer.it_interval.tv_usec = 500000;*/
 		  /* Start a virtual timer. It counts down whenever this process is
 		    executing. */
 		  //setitimer (ITIMER_VIRTUAL, &timer, NULL);
 
-		pthread_create( &communication, NULL, &getSpiData, NULL );
+		pthread_create( &communication, NULL, &manageThread, NULL );
 
-	}
+	/*}*/
 
 
 
@@ -107,16 +125,16 @@ void* sound( void* arg )
 {
 	for(int i = 0; i<50; i++)
 	{
-		digitalWrite (7, HIGH);
+		digitalWrite (REPRO, HIGH);
 		delay (4);
-		digitalWrite (7, LOW);
+		digitalWrite (REPRO, LOW);
 		delay (4);
 	}
 	for(int i = 0; i<100; i++)
 	{
-		digitalWrite (7, HIGH);
+		digitalWrite (REPRO, HIGH);
 		delay (2);
-		digitalWrite (7, LOW);
+		digitalWrite (REPRO, LOW);
 		delay (2);
 	}
 	//shutdown = false;
@@ -127,41 +145,86 @@ void endMelody(void)
 {
 	for(int i = 0; i<50; i++)
 	{
-		digitalWrite (7, HIGH);
+		digitalWrite (REPRO, HIGH);
 		delay (5);
-		digitalWrite (7, LOW);
+		digitalWrite (REPRO, LOW);
 		delay (5);
 	}
 	for(int i = 0; i<100; i++)
 	{
-		digitalWrite (7, HIGH);
+		digitalWrite (REPRO, HIGH);
 		delay (1);
-		digitalWrite (7, LOW);
+		digitalWrite (REPRO, LOW);
 		delay (1);
 	}
 	for(int i = 0; i<50; i++)
 	{
-		digitalWrite (7, HIGH);
+		digitalWrite (REPRO, HIGH);
 		delay (5);
-		digitalWrite (7, LOW);
+		digitalWrite (REPRO, LOW);
 		delay (5);
 	}
 }
 
-void* getSpiData( void* arg ){
+void* manageThread( void* arg ){
 
-	unsigned char buffer[2];
-	buffer[0] = 16;
-	unsigned short result[SAMPLE_NUM];
+	printf("manageThread started \n");
 
-	struct timeval current;
-	struct timeval start;
-	long us = 125;
-	long us2 = us;
+	while(shutdown){
+		switch (stav)
+		{
+		case SPI_SETUP:
+			if(SpiSetup() == 0){
+				stav = SET_RES;
+			}
+			else{
+				shutdown = false;
+			}
+			break;
+		case GET_DATA:
+			GetSpiData();
+			break;
+		case SET_RES:
+			SetRes();
+			stav = GET_DATA;
+			break;
+		default:
+			delay(50);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+int SpiSetup(){
+	if(wiringPiSetup() == -1)
+	{
+		printf("%s \n", "Unable to start wiringPi");
+		return 1 ;
+	}
+
+	if(wiringPiSPISetup(CHANNEL, 500000) == -1)
+	{
+		printf("%s \n", "wiringPiSPISetup Failed");
+		return 1 ;
+	}
+
+	pinMode(CS0, OUTPUT);	// 8 is GPIO number of cs of ADC
+	pinMode(CS1, OUTPUT);   // 7 is GPIO number of cs of MIC1
+	pinMode(CS2, OUTPUT);   // 7 is GPIO number of cs of MIC1
+
+	return 0;
+}
+
+int GetSpiData(void){
+	us = 125;
+	us2 = us;
 
 	gettimeofday(&start, NULL);
 
-	digitalWrite(8, 0);  // Low : CS Active
+	digitalWrite(CS0, 0);  // Low : CS Active
+	printf("pin 10 setted \n");
 	while(true)
 	{
 
@@ -174,7 +237,7 @@ void* getSpiData( void* arg ){
 		count ++;
 		if (count >= SAMPLE_NUM)
 		{
-			digitalWrite(8, 1);  // Low : CS Inactive
+			digitalWrite(CS0, 1);  // Low : CS Inactive
 			std::ofstream output_file("/home/pi/test.txt");				//Will overwrite an existing file
 			output_file << result[0];
 			output_file << " ";
@@ -193,68 +256,29 @@ void* getSpiData( void* arg ){
 			count = 0;
 		}
 
-		// Test timeru
-		/*count++;
-		if (count >= 10000)
-		{
-			printf ("%ld.%06ld: timer expired %ld times\n", start.tv_sec, start.tv_usec, count);
-			count = 0;
-		}*/
-
-		//čekání
 		do {
-	        gettimeofday(&current, NULL);
-	    } while( ( current.tv_usec + current.tv_sec*1000000 ) - ( start.tv_usec + start.tv_sec*1000000 ) <= us2 );
+			gettimeofday(&current, NULL);
+		}
+		while( ( current.tv_usec + current.tv_sec*1000000 ) - ( start.tv_usec + start.tv_sec*1000000 ) <= us2 );
+
 		us2=2*us-(( current.tv_usec + current.tv_sec*1000000 ) - ( start.tv_usec + start.tv_sec*1000000 ));
 		start = current;
-	}
-	/*while(true){
-		unsigned char buffer[2];
-		buffer[0] = 16;
-		unsigned short result[SAMPLE_NUM];
-		for(int i = 0; i <SAMPLE_NUM; i++)
-		{
-			digitalWrite(8, 0);  // Low : CS Active
-			buffer[0] = 4;	// 8 is channel 2, 4 is channel 1
-			buffer[1] = 0;
-			wiringPiSPIDataRW(CHANNEL, buffer, 2);
-			result[i] = (buffer[0] << 8) + buffer[1];
-			digitalWrite(8, 1);  // Low : CS Inactive
-			delay (1);
-		}
-		std::ofstream output_file("/home/pi/test.txt");				//Will overwrite an existing file
-		output_file << result[0];
-		output_file << " ";
-		//printf("%s %i \n", "Result[0] =", result[0]);
-		output_file.close();
 
-		std::ofstream output_file2("/home/pi/test.txt", std::ios_base::app);	//Will append to an existing file
-		for(int i = 1; i < SAMPLE_NUM; i++){
-			output_file2 << result[i];
-			output_file2 << " ";
-		}
-		output_file2.close();
-		//output_file.close();			//(Not strictly necessary as it will get closed when output_file goes out of scope)
-		//shutdown = false;
-		delay(1000);
-	}*/
-	return 0;
+	}
+
+	return 1;
 }
 
-int SpiSetup(void){
-	if(wiringPiSetup() == -1)
-	{
-		printf("%s \n", "Unable to start wiringPi");
-		return 1 ;
-	}
+int SetRes(void){
+	digitalWrite(CS1, 0); // SC of MIC1 set active
+	delay(1);
+	buffer[0] = 0b00000000;	// 48 pro psaní, + dva lsb pro nejvyšší data
+	buffer[1] = 128;
+	wiringPiSPIDataRW(CHANNEL, buffer, 2);
+	delay(10);
+	int resultOfSet = (buffer[0] << 8) + buffer[1];
+	printf ("%d\n", resultOfSet);
+	digitalWrite(CS1, 1); // SC of MIC1 set inactive
 
-	if(wiringPiSPISetup(CHANNEL, 500000) == -1)
-	{
-		printf("%s \n", "wiringPiSPISetup Failed");
-		return 1 ;
-	}
-
-	pinMode(8, OUTPUT);	// 8 is GPIO number of cs of ADC
-
-	return 0;
+	return 1;
 }
